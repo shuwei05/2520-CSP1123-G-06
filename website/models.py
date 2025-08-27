b1ce59256cdda08f5e002225e0ab7b0ea5992ab1
Yee Joe Ree <yeejo319@gmail.com>
2025-08-27 14:00:09 +0800

    Adding Database

---
+++ website/models.py
@@ -0,0 +1,25 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password1 = db.Column(db.String(150))
+    user_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+
+class Stall(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    stallname = db.Column(db.String(150), unique=True, nullable=False)
+    stallowner = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password1 = db.Column(db.String(150), nullable=False)
+    openhour = db.Column(db.Time, nullable=False)
+    closehour = db.Column(db.Time, nullable=False)
